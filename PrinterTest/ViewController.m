e489707847b945d2dc789b10afe06db346318cf0
linwei365 <linwei365@gmail.com>
2016-02-25 22:15:29 -0800

    added branch

--- PrinterTest/ViewController.m
+++ PrinterTest/ViewController.m
@@ -6,9 +6,14 @@
 //  Copyright © 2016 Lin Wei. All rights reserved.
 //
 
+#import <UIKit/UIKit.h>
+
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextView *textView;
+
+
 
 @end
 
@@ -22,6 +27,17 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (IBAction)printOnclick:(UIButton *)sender {
+//     NSURL *url = [NSURL URLWithString:self.textView.text];
+//    
+//    [UIPrinter printerWithURL:url];
+//   
+ 
+    
+    
+    NSLog(@"%@",self.textView.text);
+
 }
 
 @end
