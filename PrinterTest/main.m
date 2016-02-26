3c9600d0574b586db40edfa01166879c6ff5d24e
linwei365 <linwei365@gmail.com>
2016-02-25 20:45:54 -0800

    Initial Commit

---
+++ PrinterTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PrinterTest
+//
+//  Created by Lin Wei on 2/25/16.
+//  Copyright © 2016 Lin Wei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
